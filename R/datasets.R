052601565f9a2386b179fa7dbf834c0c2e5cbbbd
Martin Philippe-Lesaffre <56338292+MartPhilip@users.noreply.github.com>
2023-11-24 11:56:02 +0100

    data

---
+++ R/datasets.R
@@ -0,0 +1,22 @@
+#' Data: Species x Traits and Phylogeny
+#'
+#' @description
+#' This dataset is a generic dataset corresponding to species for which we
+#' various traits, continuous and categorical and phylogenetic eigenvectors.
+#' The last column is a response variable, here the number of predation events
+#' per year per species.
+#'
+#' @format A `data.frame` with 200 rows (species) and the following two
+#' columns:
+#' \describe{
+#'   \item{Species_ID}{code to identify species, each species must be unique}
+#'   \item{Numeric_Trait_X}{value of the numeric trait number X for a unique species}
+#'   \item{Categorical_Trait_X}{category of the categorical trait number X for a unique species}
+#'   \item{EigX}{values of the eigenvector number X for a unique species}
+#' }
+#'
+#' @examples
+#' 'Need to be done'
+
+
+"dataset"
